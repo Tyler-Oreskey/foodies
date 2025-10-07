b7af76c8f0751488a8481b216b76171d504cb44d
Tyler Oreskey <toreskey@gmail.com>
2025-10-06 18:19:34 -0600

    Added static and dynamic metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import { Suspense } from 'react';
 import MealsGrid from '../components/meals/meals-grid'; 
 import { getMeals } from '@/lib/meals';
 
+export const metadata = {
+  title: 'All meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
